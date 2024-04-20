be0b249078c011491a6934eb1cc39ce1749c20f7
ekwinder <ekwinder.itvedant.com>
2024-04-20 12:05:48 +0530

    initial commit

---
+++ src/app/product/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number,
+    name: string,
+    price: number,
+    code: string,
+    image: string,
+    isAvailable: boolean,
+    manufacturedDate: string,
+    description?: string
+}
